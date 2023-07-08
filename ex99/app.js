c4b9193067c0e339c1ac26c335b2c25928913760
Voilens <voilens@yandex.ru>
2023-07-08 22:42:55 +0300

    hdskj

---
+++ ex99/app.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3001
+
+app.get('/', (req, res) => {
+    res.send("Hello World!")
+})
+
+app.listen(port, () => {
+    console.log(`Web app available at http://127.0.0.1:${port}`)
+})
+
+/*
+маршруты приложения со статьями 
+POST/articles
+GET/articles/:id
+GET/articles
+DELETE/articles/:id
+*/
