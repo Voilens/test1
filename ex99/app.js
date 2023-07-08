e2d699eeb8891deed2446eac2325fd118595b431
Voilens <voilens@yandex.ru>
2023-07-09 00:01:08 +0300

    update ex99

--- ex99/app.js
+++ ex99/app.js
@@ -3,14 +3,19 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const articles = [{title: 'Example 1'}, {title: 'Example 2'}, {title: 'Example 3'}] // пока что имитация базы данных. В данном массиве будут храниться статьи ввиде объектов
+const bodyParser = require('body-parser')
 
 app.set('port', process.env.PORT || 3001) // port
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded( {extended: true} ))
 
 app.get('/articles', (req, res, next) => {
     res.send(articles)
 })
 
 app.post('/articles', (req, res, next) => {
+    const article = { title: req.body.title }
     res.send('ok')
 })
 
