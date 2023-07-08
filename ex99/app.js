a60d5d24789a7b1b5929bdec89ce51ae6fb247bb
Voilens <voilens@yandex.ru>
2023-07-08 23:12:23 +0300

    ex99 update

--- ex99/app.js
+++ ex99/app.js
@@ -2,16 +2,36 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const articles = [{title: 'Example'}] // пока что имитация базы данных. В данном массиве будут храниться статьи ввиде объектов
 
-const port = process.env.PORT || 3001
+app.set(process.env.PORT || 3001) // port
 
-app.get('/', (req, res) => {
-    res.send("Hello World!")
+app.get('/articles', (req, res, next) => {
+    res.send(articles)
 })
 
-app.listen(port, () => {
-    console.log(`Web app available at http://127.0.0.1:${port}`)
+app.post('/articles', (req, res, next) => {
+    res.send('ok')
 })
+
+app.get('/articles/:id', (req, res, next) => {
+    const id = req.params.id
+    console.log('Fetching: ', id)
+    res.send(articles[id])
+})
+
+app.delete('/articles/:id', (req, res, next) => {
+    const id = req.params.id
+    console.log('Deleting: ', id)
+    delete articles[id]
+    res.send({ message: 'Deleted' })
+})
+
+app.listen(app.get('port'), () => {
+    console.log(`Web app available at http://127.0.0.1:${app.get('port')}`)
+})
+
+module.exports = app
 
 /*
 маршруты приложения со статьями 
